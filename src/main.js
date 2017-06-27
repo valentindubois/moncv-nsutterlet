83bf1818bb5e3afe8e7960076be0c0ec5581c571
nsutterlet <n.sutterlet@outlook.com>
2017-06-27 20:06:32 +0200

    version initial avec config jQuerry

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootswatch/superhero/bootstrap.min.css';
+import './main.css';
+$(document).ready(function () {
+    console.log('it works!');
+});
